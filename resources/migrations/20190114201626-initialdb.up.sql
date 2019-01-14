742f83acea2810de97a5b33b0485b61cb97c98b6
Chad Stovern <chad@stovern.me>
2019-01-14 14:24:04 -0600

    initial db setup

---
+++ resources/migrations/20190114201626-initialdb.up.sql
@@ -0,0 +1,7 @@
+create extension if not exists "uuid-ossp";
+--;;
+create table if not exists messages (
+id uuid primary key default uuid_generate_v4(),
+message text not null,
+date_created timestamptz not null default now()
+);
