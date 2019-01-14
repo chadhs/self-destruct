a326dc3b061a140d542038a1d17a92ea1a974794
Chad Stovern <chad@stovern.me>
2019-01-14 14:28:52 -0600

    typo fix

--- resources/migrations/20190114201626-initialdb.up.sql
+++ resources/migrations/20190114201626-initialdb.up.sql
@@ -1,6 +1,6 @@
 create extension if not exists "uuid-ossp";
 --;;
-create table if not exists messages (
+create table if not exists message (
 id uuid primary key default uuid_generate_v4(),
 message text not null,
 date_created timestamptz not null default now()
